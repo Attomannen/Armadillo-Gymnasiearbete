3d0d67f4977bae9e3a2a5b0b397545daaf03f838
Attomannen <antonb0425@gmail.com>
2022-09-19 19:14:21 +0200

    Working Project

---
+++ Armadillo/Assets/Movement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] float playerSpeed = 2.0f;
+    [SerializeField] float jumpHeight = 1.0f;
+    [SerializeField] float gravityValue = -9.81f;
+    [SerializeField] float rotationSpeed = 5f;
+    PlayerInput input;
+    InputAction moveAction;
+    InputAction jumpAction;
+    private Transform camTransform;
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        camTransform = Camera.main.transform;
+        input = GetComponent<PlayerInput>();
+        controller = GetComponent<CharacterController>();
+        moveAction = input.actions["Move"];
+        jumpAction = input.actions["Jump"];
+
+
+    }
+    private Vector2 currentInputVector;
+    private Vector2 smoothInputVelocity;
+    [SerializeField] float smoothInputSpeed = 0.2f;
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        currentInputVector = Vector2.SmoothDamp(currentInputVector, input, ref smoothInputVelocity, smoothInputSpeed);
+        Vector3 move = new Vector3(currentInputVector.x, 0, currentInputVector.y);
+
+
+        move = move.x * camTransform.right.normalized + move.z * camTransform.forward.normalized;
+
+        move.y = 0f;
+
+        anim.SetFloat("Blend", currentInputVector.magnitude, 1, 2);
+        if (groundedPlayer)
+        {
+            smoothInputSpeed = 0.2f;
+        }
+        else
+        {
+            smoothInputSpeed = 0.6f;
+        }
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+
+
+
+
+
+
+        // Changes the height position of the player..
+        if (jumpAction.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+        float targetAngle = camTransform.rotation.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, targetAngle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
+
+    }
+}
---
+++ Armadillo/Assets/Scripts/SwitchForms.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class SwitchForms : MonoBehaviour
+{
+    [SerializeField] GameObject Normal;
+    [SerializeField] GameObject Ball;
+    //public bool isWerewolf;
+    public int formSwitch { get; set; }
+    PlayerInput input;
+    InputAction modeAction;
+    CharacterController controller;
+    BallMovement ballScript;
+    Movement normalMovement;
+    ShootScript shoot;
+    private void Awake()
+    {
+        shoot = GetComponent<ShootScript>();
+        normalMovement = GetComponent<Movement>();
+        ballScript = GetComponent<BallMovement>();
+        controller = GetComponent<CharacterController>();
+        input = GetComponent<PlayerInput>();
+        
+        Normal.SetActive(true);
+        Ball.SetActive(false);
+        modeAction = input.actions["SwitchMode"];
+        formSwitch = 1;
+    }
+
+
+    private void Update()
+    {
+        Transformation();
+    }
+
+    public void Transformation()
+    {
+        float action = modeAction.ReadValue<float>();
+        if (action == 1)
+        {
+            BallArmadillo();
+        }
+
+        else if (action != 1)
+        {
+            NormalArmadillo();
+        }
+    }
+
+
+    void NormalArmadillo()
+    {
+            Normal.SetActive(true);
+            Ball.SetActive(false);
+            formSwitch = 1;
+            ballScript.enabled = false;
+            normalMovement.enabled = true;
+            shoot.enabled = true;
+            Normal.transform.position = Ball.transform.position;
+            controller.height = 1.5f;
+    }
+
+    void BallArmadillo()
+    {
+        Normal.SetActive(false);
+        Ball.SetActive(true);
+        formSwitch = 2;
+        ballScript.enabled = true;
+        normalMovement.enabled = false;
+        shoot.enabled = false;
+        Ball.transform.position = Normal.transform.position;
+        Ball.transform.rotation = Normal.transform.rotation;
+        controller.height = 1f;
+    }
+}
