d8401e4fd04ce9e56e88e2aa0fef8bf54cc9c09c
Attomannen <antonb0425@gmail.com>
2022-12-20 00:00:56 +0100

    Update EnemyWaveSpawner.cs

--- ArmaDillo/Assets/Scripts/EnemyWaveSpawner.cs
+++ ArmaDillo/Assets/Scripts/EnemyWaveSpawner.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class EnemyWaveSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject enemyPrefab;
+
+    [SerializeField] float spawnInterval = 1.0f;
+
+    [SerializeField] int enemiesPerWave = 10;
+
+    [SerializeField] int maxWaves = 10;
+
+    [SerializeField] int increaseWave = 5;
+
+    [SerializeField] int additionalEnemiesPerWave = 5;
+
+    [SerializeField] List<Transform> spawnPoints;
+
+    private int currentWave = 0;
+
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemies()
     {
-        
+        while (currentWave < maxWaves)
+        {
+            int enemiesToSpawn = enemiesPerWave;
+            if (currentWave >= increaseWave)
+            {
+                enemiesToSpawn += additionalEnemiesPerWave;
+            }
+
+            for (int i = 0; i < enemiesToSpawn; i++)
+            {
+                Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+
+                Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            }
+
+            currentWave++;
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
     }
 }
